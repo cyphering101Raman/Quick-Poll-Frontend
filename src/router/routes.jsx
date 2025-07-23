a2c7d175c8e0f7c7de8195a72806eac528d7cd7e
cyphering101Raman <guptaraman600@gmail.com>
2025-07-24 00:10:23 +0530

    Add Router

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 
 import App from '../App'
+import { Home, Explore } from "../pages/index.js"
+
+import PollCard from '../components/PollCard.jsx'
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route path='/explore' element={<></>}/>
+      <Route index element={<Home/>}/>
+      <Route path='/pollcard' element={<PollCard />}/>
+      <Route path='/explore' element={<Explore />}/>
       <Route path='/contact-us' element={<></>}/>
+      <Route path='/dashboard' element={<></>}/>
+      <Route path='/profile' element={<></>}/>
       <Route path='/login' element={<></>}/>
       <Route path='/signup' element={<></>}/>
+      <Route path='*' element={<></>}/>
     </Route>
   )
 )
