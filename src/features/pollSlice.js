7c8df143eca5978b765e30726104d875e47cdfde
cyphering101Raman <guptaraman600@gmail.com>
2025-07-25 23:20:51 +0530

    Add poll slice and integrate with store

---
+++ src/features/pollSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  pollArray: [],
+}
+
+const pollSlice = createSlice({
+  name: "pollData",
+  initialState,
+  reducers: {
+    addPoll: (state, action) => {    // add poll to List of Polls
+      state.pollArray.push(action.payload);
+    },
+    loadPolls: (state, action) => {     //loading all the polls for refresh
+      state.pollArray = action.payload;
+    },
+    castVote: (state, action) => {
+      const { pollId, optionIdx, userId } = action.payload;
+      const poll = state.pollArray.find(poll => poll.id === pollId)
+      if (poll) {
+        poll.options[optionIdx].voteCount += 1;
+        poll.votedUsers.push(userId);
+      }
+    },
+    deletePoll: (state, action) => {    // deleting the poll
+      const pollId = action.payload;
+      state.pollArray = state.pollArray.filter(p => p.id !== pollId)
+      state.activePoll = null;
+    }
+  }
+})
+
+export const { addPoll, loadPolls, castVote, deletePoll } = pollSlice.actions;
+
+export default pollSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "../features/authSlice.js"
+import pollReducer from "../features/pollSlice.js"
 
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    poll: pollReducer,
   }
 })
 
