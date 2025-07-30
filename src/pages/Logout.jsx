4a96ffb2867c08ec5584a77ab4cf85becaff8176
cyphering101Raman <guptaraman600@gmail.com>
2025-07-30 12:56:58 +0530

    Add Logout Backend Api

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,21 +1,30 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import {logout} from "../features/authSlice.js"
-import { removeActiveUser } from '../utils/localStorage'
+import { logout } from "../features/authSlice.js"
+// import { removeActiveUser } from '../utils/localStorage'
 import { useNavigate } from 'react-router-dom'
+import axiosInstance from '../utils/axiosInstance.js'
 
 const Logout = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate();
 
   useEffect(() => {
-    dispatch(logout());
-    removeActiveUser();
-    navigate('/');
-  }, [])
-  
+    const handleLogout = async () => {
+      try {
+        const res = await axiosInstance.post("/users/logout")
+        dispatch(logout());
+        navigate('/');
+      } catch (error) {
+        console.error("Logout error:", error)
+      }
+    }
+
+    handleLogout()
+  }, [dispatch, navigate])
+
   return (
-    <div>Logout</div>
+    <></>
   )
 }
 
