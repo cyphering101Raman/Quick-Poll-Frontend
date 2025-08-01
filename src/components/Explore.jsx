a9c28c15b9e898843763efbbb91988126da380ca
cyphering101Raman <guptaraman600@gmail.com>
2025-08-01 22:34:04 +0530

    Update Explore with Loading Screen

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -7,6 +7,7 @@ const Explore = () => {
 
   const [activePolls, setActivePolls] = useState([]);
   const [expiredPolls, setExpiredPolls] = useState([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchPolls = async () => {
@@ -16,16 +17,26 @@ const Explore = () => {
 
         setActivePolls(activePolls);
         setExpiredPolls(expiredPolls);
-
+        setLoading(false)
+        
       } catch (error) {
         console.error("Failed to fetch polls:", error);
+        setLoading(false)
       }
     }
     fetchPolls();
   }, [])
 
+  if (loading) {
+    return (
+      <main className="min-h-screen bg-gradient-to-r from-purple-700 to-blue-600  flex items-center justify-center text-white font-semibold">
+        <p className="text-2xl text-gray-200 tracking-widest animate-bounce">L o a d i n g . . .</p>
+      </main>
+    );
+  }
+
   return (
-    <section className="min-h-screen bg-gradient-to-r from-purple-800 via-indigo-600 to-sky-500 py-12 px-4">
+    <section className="min-h-[screen] bg-gradient-to-r from-purple-800 via-indigo-600 to-sky-500 py-12 px-4">
 
       {/* Explore Section Heading */}
       <div className="max-w-5xl mx-auto text-center mb-12">
@@ -34,21 +45,21 @@ const Explore = () => {
       </div>
 
       {/* Poll Cards */}
-      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 px-4">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-9 gap-y-12 px-4">
         {activePolls?.map((poll) => (
-          <PollCard key={poll._id} {...poll} />
+          <PollCard key={poll._id} {...poll} showPollBtn={true}/>
         ))}
       </div>
 
       {/* Expired Polls Section */}
-      {expiredPolls.length === 0 && (
+      {expiredPolls.length > 0 && (
         <div className="mt-20 max-w-5xl mx-auto text-center">
           <h2 className="text-3xl font-bold text-white mb-1">Past Polls</h2>
           <p className="text-gray-300 text-base mb-8">These polls have concluded. View results below.</p>
 
-          <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 px-4">
+          <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 px-4">
             {expiredPolls.map((poll) => (
-              <PollCard key={poll._id} {...poll} />
+              <PollCard key={poll._id} {...poll} showPollBtn={true} />
             ))}
           </div>
         </div>
